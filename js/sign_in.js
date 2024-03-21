26e4b74c768099ef05c04d5e70de86f10cc09031
emmanuellumwachi <lumwachiemmanuel18@gmail.com>
2024-03-21 12:11:12 +0300

    added styles

--- js/sign_in.js
+++ js/sign_in.js
@@ -1,40 +1,74 @@
-function validateForm() {
-    var username = document.querySelector('.sign_in input[type="text"]').value.trim();
-    var password = document.querySelector('.sign_in input[type="password"]').value.trim();
+
+const inputs=document.querySelectorAll("input");
+console.log(inputs)
+const submitbtn=document.querySelector(".submitbtn")
+console.log(submitbtn)
+const forgotbtn=document.querySelector(".forgotbtn")
+console.log(forgotbtn)
 
 
-    if (username === '' || password === '') {
-        alert("Please fill in all fields.");
-        return false; 
-    }
+const signin_error=document.querySelector(".signin-error")
+
+console.log(signin_error)
+submitbtn.addEventListener("click",function(e){
+    e.preventDefault()
+    inputs.forEach((input)=>{
+
+        if(input.value===""){
+            signin_error.textContent="Kindly fill all fields"
+            input.style.border="2px solid red" 
+        }
+    })
+    console.log(signin_error)
+})
+
+
+
+
+
+
+
+
+
+
+// function validateForm() {
+//     var username = document.querySelector('.sign_in input[type="text"]').value.trim();
+//     var password = document.querySelector('.sign_in input[type="password"]').value.trim();
+
+
+//     if (username === '' || password === '') {
+//         alert("Please fill in all fields.");
+//         return false; 
+//     }
 
     
-    if (username === "correct_username" && password === "correct_password") {
-        alert("Login successful!");
-        localStorage.setItem(username," ")
-        localStorage.getItem(username," ")
-        return true;
-    } else {
-        alert("Incorrect username or password. Please try again.");
-        return false; 
-    }
-}
+//     if (username === "correct_username" && password === "correct_password") {
+//         alert("Login successful!");
+//         localStorage.setItem(username," ")
+//         localStorage.getItem(username," ")
+//         return true;
+//     } else {
+//         alert("Incorrect username or password. Please try again.");
+//         return false; 
+//     }
+// }
 
 
-function resetPassword() {
-    var username = document.querySelector('.sign_in input[type="text"]').value.trim();
-
-    
-    if (username === '') {
-        alert("Please fill in your username to reset password.");
-        return false;
-    }
+// function resetPassword() {
+//     var username = document.querySelector('.sign_in input[type="text"]').value.trim();
 
     
-    alert("Password reset link has been sent to your email.");
-    return true;
-}
+//     if (username === '') {
+// signin_error.textContent="Kindly fill all fields"
+//         // alert("Please fill in your username to reset password.");
+//         // return false;
+//     }
+
+    
+//     // alert("Password reset link has been sent to your email.");
+//     // return true;
+// }
 
 
-document.querySelector('.btn button:first-of-type').addEventListener('click', validateForm);
-document.querySelector('.btn button:last-of-type').addEventListener('click', resetPassword);
+// document.querySelector('.btn button:first-of-type').addEventListener('click', validateForm);
+// document.querySelector('.btn button:last-of-type').addEventListener('click', resetPassword);>
--- js/sign_up.js
+++ js/sign_up.js
@@ -1,10 +1,2 @@
- 
- const storedProfile = localStorage.getItem('userProfile');
-
- if (storedProfile) {
-
-     window.location.href = `profile.html`;
- } else {
-
-     window.location.href = 'sign_up.html';
- }
+const inputs=document.querySelectorAll(".signupinputs");
+console.log(inputs)
